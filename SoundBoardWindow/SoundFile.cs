cca2714f1c15166efa8448754653a3a3863cc5c5
Gabriel Lundgren <glundgren@my.apsu.edu>
2023-04-14 15:48:42 -0500

    fixed the cs files to be in the solution with the SoundBoardWindow so they can actually communicate with each other

---
+++ SoundBoardWindow/SoundFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SoundFile
+{
+	private string _name;
+	private string _filePath;
+    private int _soundLength;
+    private List<string> _tags;
+
+    public SoundFile()
+	{
+
+	}
+
+	public SoundFile(string name, string filePath, int soundLength, List<string> tags)
+	{
+
+	}
+}
---
+++ SoundBoardWindow/StateMachine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+/// <summary>
+/// Holds current general use states for the application
+/// </summary>
+public class StateMachine
+{
+
+	private List<String> _soundFiles;
+	public StateMachine()
+	{
+	}
+}
