1d18f9726660957277204e6fab5f77770ba99ceb
Gabriel Lundgren <glundgren@my.apsu.edu>
2023-04-17 14:43:49 -0500

    experimenting

--- SoundBoardWindow/SoundFile.cs
+++ SoundBoardWindow/SoundFile.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Media;
+using System.Windows.Media;
 
 public class SoundFile
 {
-	private string _name;
-	private string _filePath;
-    private int _soundLength;
-    private List<string> _tags;
+	private string _name { get; set; }
+	private string _filePath { get; set; }
+    private int _soundLength {  get; set; }
+    private List<string> _tags {  get; set; }
+    private Uri uri { get; set; }
 
-    public SoundFile()
+    /// <summary>
+    /// Receive information from user input and then create a sound file object 
+    /// </summary>
+    /// <param name="name">name of soundFile</param>
+    /// <param name="filePath">path to reach file stored locally</param>
+    /// <param name="soundLength"></param>
+    /// <param name="tags"></param>
+    public SoundFile(string name, string filePath, int soundLength, List<string> tags)
 	{
+        MediaPlayer player2 = new MediaPlayer();
 
 	}
 
-	public SoundFile(string name, string filePath, int soundLength, List<string> tags)
-	{
-
-	}
 }
